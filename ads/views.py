69c96c261b64672deba2317daa072f6bf9f04a7d
Artem <artem.mikhaylov5593@yandex.ru>
2022-06-26 20:02:24 +0300

    created advertising app

--- ads/views.py
+++ ads/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
+
+from ads.models import Ad
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class AdView(View):
+
+    def get(self, request):
+        ads = Ad.objects.all()
+
+        response = []
+        for ad in ads:
+            response.append({
+                "id": ad.id,
+                "name": ad.name,
+                "author": ad.author,
+                "price": ad.price,
+            })
+
+        return JsonResponse(response, safe=False)
+
+    def post(self, request):
+        ad_data = json.loads(request.body)
+
+        ad = Ad.objects.create(
+            name=ad_data["name"],
+            author=ad_data["author"],
+            price=ad_data["price"],
+            description=ad_data["description"],
+            address=ad_data["address"],
+            is_published=ad_data["is_published"],
+        )
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.adress,
+            "is_published": ad.is_published,
+        }, safe=False)
+
+
+class AdDetailView(DetailView):
+    model = Ad
+
+    def get(self, request, *args, **kwargs):
+        ad = self.get_object()
+
+        return JsonResponse({
+            "id": ad.id,
+            "name": ad.name,
+            "author": ad.author,
+            "price": ad.price,
+            "description": ad.description,
+            "address": ad.adress,
+            "is_published": ad.is_published,
+        })
