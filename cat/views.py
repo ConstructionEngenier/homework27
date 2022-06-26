5838696134b11bf362d5b1d165c01a751888bd07
Artem <artem.mikhaylov5593@yandex.ru>
2022-06-26 20:02:38 +0300

    created categories app

--- cat/views.py
+++ cat/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import DetailView
+
+from cat.models import Category
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class CategoryView(View):
+
+    def get(self, request):
+        categories = Category.objects.all()
+
+        response = []
+        for category in categories:
+            response.append({
+                "id": category.id,
+                "name": category.name,
+            })
+
+        return JsonResponse(response, safe=False)
+
+    def post(self, request):
+        category_data = json.loads(request.body)
+
+        category = Category.objects.create(
+            name=category_data["name"]
+        )
+        return JsonResponse({
+            "id": category.id,
+            "name": category.name,
+        }, safe=False)
+
+
+class CategoryDetailView(DetailView):
+    model = Category
+
+    def get(self, request, *args, **kwargs):
+        category = self.get_object()
+
+        return JsonResponse({
+            "id": category.id,
+            "name": category.name,
+        })
